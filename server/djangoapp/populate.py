72075402667736c6083d7c44ca0e6559196ad781
Gladys <99319413+Gladys-tech@users.noreply.github.com>
2025-06-02 15:49:38 +0300

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,6 +1,3 @@
-# def initiate():
-#     print("Populate not implemented. Add data manually")
-
 from .models import CarMake, CarModel
 
 
@@ -15,14 +12,12 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(
-            CarMake.objects.create(
-                name=data['name'],
-                description=data['description']
-            )
+        instance = CarMake.objects.create(
+            name=data["name"],
+            description=data["description"]
         )
+        car_make_instances.append(instance)
 
-    # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
         {"name": "Pathfinder", "type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
         {"name": "Qashqai", "type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
@@ -39,13 +34,12 @@ def initiate():
         {"name": "Corolla", "type": "Sedan", "year": 2023, "car_make": car_make_instances[4]},
         {"name": "Camry", "type": "Sedan", "year": 2023, "car_make": car_make_instances[4]},
         {"name": "Kluger", "type": "SUV", "year": 2023, "car_make": car_make_instances[4]},
-        # Add more CarModel instances as needed
     ]
 
     for data in car_model_data:
         CarModel.objects.create(
-            name=data['name'],
-            car_make=data['car_make'],
-            type=data['type'],
-            year=data['year']
+            name=data["name"],
+            car_make=data["car_make"],
+            type=data["type"],
+            year=data["year"]
         )
